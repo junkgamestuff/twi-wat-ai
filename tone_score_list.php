a5c093ae72114018b0bc83e54661015dd21c3ff1
Junkgamestuff <mjuarez@coscient.com>
2019-11-13 15:01:18 -0800

    Small cleanup
    Clean up spaces

--- tone_score_list.php
+++ tone_score_list.php
@@ -1,16 +1,9 @@
 <?php
-
 error_reporting(E_ALL);
 
 include "includes/connection.php";
-
-
 $src = $_GET["src"];
 $name = $_GET["name"];
-
-//$src = "anime";
-//$name = "Anger";
-
 ?>
 
 
@@ -19,11 +12,7 @@ $name = $_GET["name"];
 	<title></title>
 </head>
 <body>
-
-
 <?php
-
-
 $score = array();
 $sql="SELECT * FROM twitter_tone_scores WHERE name = '" . $name . "' AND src = '" . $src . "'";
 $rs=$mysqli->query($sql);
@@ -32,17 +21,11 @@ while($row = $rs->fetch_assoc()) {
 	
 	$score[] = $row["score"];
 }
-
 count($score);
 $score = array_filter($score);
 $score = array_sum($score)/count($score);
 $score = round((float)$score * 100) . '%';
 ?>
-
-
 <?php echo $name; ?> - <?php echo $score; ?>
-
-
 </body>
 </html>
-
